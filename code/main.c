653d57631229d15b9dd4ff656339307b34caff26
Alex <farenhite451@gmail.com>
2016-05-03 11:33:46 -0400

    Integrated mic testing. Screen functions currently commented out

--- code/main.c
+++ code/main.c
@@ -18,26 +18,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "mic/microphone_functions.h"
+#include "mic/sound_loudness.h"
+#define AUDIO_BUF_SIZE 2048
+float audio_buffer[AUDIO_BUF_SIZE];
+
 int main(void)
 {
 	//Initialize system
 	SystemInit();
-	
-	//Initialize ILI9341
+	//Init mic
+	microphone_init();
+	microphone_start();
+        /*/Initialize ILI9341
 	TM_ILI9341_Init();
 	//Rotate LCD for 90 degrees
 	TM_ILI9341_Rotate(TM_ILI9341_Orientation_Landscape_2);
 	//FIll lcd with color
-	TM_ILI9341_Fill(ILI9341_COLOR_MAGENTA);
-	//Draw white circle
-	TM_ILI9341_DrawCircle(60, 60, 40, ILI9341_COLOR_GREEN);
-	//Draw red filled circle
-	TM_ILI9341_DrawFilledCircle(60, 60, 35, ILI9341_COLOR_RED);
-	//Draw blue rectangle
-	TM_ILI9341_DrawRectangle(120, 20, 220, 100, ILI9341_COLOR_BLUE);
-	//Draw black filled rectangle
-	TM_ILI9341_DrawFilledRectangle(130, 30, 210, 90, ILI9341_COLOR_BLACK);
-	//Draw line with custom color 0x0005
 	TM_ILI9341_DrawLine(10, 120, 310, 120, 0x0005);
 	
 	//Put string with black foreground color and blue background with 11x18px font
@@ -46,11 +43,46 @@ int main(void)
 	TM_ILI9341_Puts(60, 150, "FINDER", &TM_Font_11x18, ILI9341_COLOR_BLACK, ILI9341_COLOR_BLUE2);
 	//Put string with black foreground color and red background with 11x18px font
 	TM_ILI9341_Puts(245, 225, "majerle.eu", &TM_Font_7x10, ILI9341_COLOR_BLACK, ILI9341_COLOR_ORANGE);
- 
-    //Initialize system
-    SystemInit();
+*/
+	uint32_t audio_buffer_index = 0; // index variable
+	float sound_loudness;
+	int mod = 0;
+	while (1)
+	{
+		// read one array of samples from the microphone
+	uint32_t n_samples; // number of samples read in one batch from the microphone
+	uint16_t *audio_samples;
+	while (1)
+	{
+		audio_samples = microphone_get_data_if_ready(&n_samples); // try to read an array of samples from the microphone
+		if (audio_samples != 0) break;       // if array of samples was read
+	}
+	
+	// copy array of samples from microphone into our array audio_buffer
+	for(int i = 0; i < n_samples; i++){
+			audio_buffer[audio_buffer_index ++] = (int16_t) audio_samples[i];
+		  if (audio_buffer_index >= AUDIO_BUF_SIZE) break;
+	}
+	
+	// calculate sound loudness 
+	if (audio_buffer_index >= AUDIO_BUF_SIZE)
+	{
+                 
+		 sound_loudness = calculate_sound_loudness(audio_buffer, AUDIO_BUF_SIZE);
+      
+			uint32_t duty;
+			if(sound_loudness < 500)
+				duty = 5;
+			else if(sound_loudness > 30000)
+				duty = 90;
+			else
+				duty = 5 + 85 * (sound_loudness - 500) / 25000;
 
-	while (1) {
+                        if(!((mod++)%10))
+			  printf("Loudness:%d\n",duty);
+			  //printf("%s", "ad\n");
+			audio_buffer_index  = 0;
+		}
 	}
 }
 
