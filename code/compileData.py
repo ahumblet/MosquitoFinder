81f337e33167b82669730623325406e9827a3d9c
Alex <farenhite451@gmail.com>
2016-05-17 08:12:06 -0400

    Day 811: weka file generation close to complete

--- code/compileData.py
+++ code/compileData.py
@@ -1,5 +1,22 @@
 import arff
 import pickle
 
-p = pickle.load(open("1.p","rb"))
-print(p)
+attributes = []
+nFFT = 64
+baseStr = "FFT_"
+for i in range(nFFT):
+    name = baseStr + str(i)
+    attributes.append(name)
+attributes.append("Dist")
+relation = "Dist_FFT"
+
+data = []
+samples = 120
+for i in range(samples):
+    filename = str(i+1) + ".p"
+    f = open(filename,"rb")
+    p = pickle.load(f)
+    f.close()
+    p.pop()
+    data.append(p)
+print(data)
