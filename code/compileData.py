24a4fbc7e05188d06f494b658b2f92d89043d6b6
Alex <farenhite451@gmail.com>
2016-05-17 08:48:36 -0400

    Day 848: NEURAL NET MODELS CREATED, 10% training error 0.o

--- code/compileData.py
+++ code/compileData.py
@@ -6,17 +6,23 @@ nFFT = 64
 baseStr = "FFT_"
 for i in range(nFFT):
     name = baseStr + str(i)
-    attributes.append(name)
-attributes.append("Dist")
+    attributes.append((name,u'REAL'))
+attributes.append(("Ang",u'REAL'))
+#print(attributes)
 relation = "Dist_FFT"
 
-data = []
+dataF = []
 samples = 120
 for i in range(samples):
     filename = str(i+1) + ".p"
     f = open(filename,"rb")
     p = pickle.load(f)
     f.close()
-    p.pop()
-    data.append(p)
-print(data)
+    p.pop(-2)
+    dataF.append(p)
+
+data = {u'attributes': attributes, u'data': dataF, u'description': u'', u'relation': relation}
+
+f = open("myData2.arff","w")
+f.write(arff.dumps(data))
+f.close()
