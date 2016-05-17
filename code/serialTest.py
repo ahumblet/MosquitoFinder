0019cfbe7068cb8662f4af17a5bbee3912a07615
Alex <farenhite451@gmail.com>
2016-05-17 07:46:56 -0400

    Day 746: sample gathering is finished

--- code/serialTest.py
+++ code/serialTest.py
@@ -2,26 +2,31 @@ import serial
 import arff
 import pickle
 import struct
-
+NSAMP = 10
 ser = serial.Serial('/dev/ttyACM0')
 #input("Press Enter to Start...")
-num = input("Sample number: ")
-file = str(num)
-file = file + ".p"
-print(file)
+num = input("Start number: ")
+num = int(num) - 1
 dist = input("Distance: ")
 angle = input("Angle: ")
-startByte = b'a'
-ser.write(startByte)
-g = ser.read(4)
-length = struct.unpack('i',g)
-print("The length is ")
-print(length[0])
-q = []
-for i in range(length[0]):
-        print("Reading Index: ",i)
+for i in range(NSAMP):
+        num = num + 1
+        file = str(num)
+        file = file + ".p"
+        print(file)
+        startByte = b'a'
+        ser.write(startByte)
         g = ser.read(4)
-        s = struct.unpack('f',g)
-        q.append(s[0])
-print(q)
-pickle.dump(q,open(file,"wb"))
+        length = struct.unpack('i',g)
+        print("The length is ")
+        print(length[0])
+        q = []
+        for i in range(length[0]):
+                print("Reading Index: ",i)
+                g = ser.read(4)
+                s = struct.unpack('f',g)
+                q.append(s[0])
+        print(q)
+        q.append(float(dist))
+        q.append(float(angle))
+        pickle.dump(q,open(file,"wb"))
